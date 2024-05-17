f714d38986729e0805fdae1861e815d0eeeb7c4d
shreyas1817 <165317781+shreyas1817@users.noreply.github.com>
2024-05-17 23:29:21 +0530

    Update bill.c

--- Billing System/bill.c
+++ Billing System/bill.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include "bill.h"
 
-void printBill(struct Customer customer, struct Item items[], int quantities[], int numItems, float total) {
+void printBill(char username[], struct Item items[], int quantities[], int numItems, float total) {
     printf("\nCustomer Information:\n");
-    printf("ID: %d\n", customer.id);
-    printf("Name: %s\n", customer.name);
-    printf("Contact: %s\n", customer.contact);
+    printf("Name: %s\n", username);
     printf("\nTotal Bill: Rs%.2f\n", total);
 }
