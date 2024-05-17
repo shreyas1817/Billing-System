aa53e59145a472893956aa7049338d0c9f640453
shreyas1817 <165317781+shreyas1817@users.noreply.github.com>
2024-05-17 23:31:20 +0530

    Update customer.h

--- Billing System/customer.h
+++ Billing System/customer.h
@@ -1,12 +1,8 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
-struct Customer {
-    int id;
-    char name[50];
-    char contact[20];
-};
 
-void storeCustomer(struct Customer *customer);
+void store_user(const char *username, const char *password);
+int check_user(const char *username, const char *password);
 
 #endif
